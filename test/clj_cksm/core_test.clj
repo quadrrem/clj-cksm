cdff294704df39bd5814e7da5cffdb0ffbaaa08b
Remo Quadrelli <remo.quadrelli@gmail.com>
2018-12-02 18:39:59 +0100

    fix test descriptions

--- test/clj_cksm/core_test.clj
+++ test/clj_cksm/core_test.clj
@@ -8,9 +8,9 @@
 
 
 (deftest sha1
-  (testing "MD5 checksum"
+  (testing "SHA1 checksum"
     (is (= "042244B2F192294D7322CE9492E044B39322250E" (toHex (digest (.getBytes "TestString1") "SHA1"))))))
 
 (deftest sha-256
-  (testing "MD5 checksum"
+  (testing "SHA-256 checksum"
     (is (= "230B94A79B35A026063A4B87469B4C42836E1C30CA13D5DC4AB42563A4D13B72" (toHex (digest (.getBytes "TestString1") "SHA-256"))))))    
